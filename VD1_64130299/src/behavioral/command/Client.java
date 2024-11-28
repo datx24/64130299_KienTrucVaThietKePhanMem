2628af77f28d71827fc120522c460a7c1f3fbbc2
datx24 <xuandat475@gmail.com>
2024-11-28 14:29:38 +0700

    viết hàm main để test

---
+++ VD1_64130299/src/behavioral/command/Client.java
@@ -0,0 +1,22 @@
+package behavioral.command;
+
+public class Client {
+    public static void main(String[] args) {
+        CasioCalculator calculator = new CasioCalculator();
+
+        calculator.compute('+',10);
+        System.out.println(calculator.getResult());
+
+        calculator.compute('*',2);
+        System.out.println(calculator.getResult());
+
+        calculator.undo();
+        System.out.println(calculator.getResult());
+
+        calculator.redo();
+        System.out.println(calculator.getResult());
+
+        calculator.clear();
+        System.out.println(calculator.getResult());
+    }
+}
