c4cd283f5053b860a07fd6fd8f3198509556a86f
datx24 <xuandat475@gmail.com>
2024-11-28 14:14:42 +0700

    cài đặt caculator command

---
+++ VD1_64130299/src/behavioral/command/CalculatorCommand.java
@@ -0,0 +1,34 @@
+package behavioral.command;
+
+public class CalculatorCommand implements ICommand{
+    private char operator;
+    private float operand;
+    private CommandHandler handler;
+
+    public CalculatorCommand(char operator, float operand, CommandHandler handler) {
+        this.operator = operator;
+        this.operand = operand;
+        this.handler = handler;
+    }
+
+    @Override
+    public void excute() {
+        handler.handle(operator,operand);
+    }
+
+    @Override
+    public void unexcute() {
+        char undoOperator = undoOperator();
+        handler.handle(undoOperator,operand);
+    }
+
+    public char undoOperator() {
+        switch (operator){
+            case'+': return '-';
+            case'-': return '+';
+            case'*': return '/';
+            case'/': return '*';
+        }
+        return '=';
+    }
+}
